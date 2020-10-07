0a7de83a09c711de973e9b1e6cf3bfe786716005
Christian Semedo <christian.semedo@daviestech.org>
2020-10-07 13:27:23 +0000

    done

---
+++ silly_sentences.py
@@ -0,0 +1,16 @@
+name = input("Enter a name: ")
+adj = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+food = input("Enter a food: ")
+food2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the\n local cuisine, including " + adj + " " + food + " and " + food2 + ".\n")
+
+print(name + " will have to practice the language " + adverb + " to make it easier to \n" + verb + " with people.\n")
+
+print(name + " has a long list of sights to see, including the \n" + noun + " museum and the " + adj2 + " park.")
